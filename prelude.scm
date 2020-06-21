c90a87a338a544c0d6220a1c2b78774f138624e6
Alvin Kerber <alvin@alvinkerber.net>
2020-06-20 20:42:26 -0700

    hacky type operator table implementation in prelude

--- prelude.scm
+++ prelude.scm
@@ -14,3 +14,19 @@
 ;   However, they are strongly typed in this version of MIT
 ;   Scheme, so just use #t and #f as appropriate.
 
+; data-directed programming
+(define type-operator-table '())
+
+(define (put type op item)
+  (set!
+    type-operator-table
+    (cons (list type op item) type-operator-table)))
+
+(define (get type op)
+  (get-from type op type-operator-table))
+(define (get-from type op tbl)
+  (cond ((null? tbl) (error "Type-operator not found!" type op))
+        ((and (eq? type (caar tbl))
+              (eq? op (cadar tbl)))
+         (caddar tbl))
+        (else (get-from type op (cdr tbl)))))
